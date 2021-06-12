0d2be948628bfdf1e3f2f944a869cd34a14086c7
elias.salame <e>
2021-06-12 15:26:26 +0300

    push

--- src/components/partner/PartnerItem.js
+++ src/components/partner/PartnerItem.js
@@ -1,17 +1,9 @@
-import classes from "./PartnerItem.module.css";
-import Card from "../ui/Card";
 function PartnerItem(props) {
   return (
-    <li className={classes.item}>
-      <div className={classes.content}>
-        <h3>{props.organization}</h3>
-        <ul>
-          <address>{props.address}</address>
-        </ul>
-      </div>
-
-      <br />
-    </li>
+    <tr>
+      <td>{props.organization}</td>
+      <td>{props.address}</td>
+    </tr>
   );
 }
 export default PartnerItem;
--- src/components/partner/PartnerList.js
+++ src/components/partner/PartnerList.js
@@ -1,16 +1,28 @@
 import PartnerItem from "./PartnerItem";
 import classes from "./PartnerList.module.css";
+import { Table } from "react-bootstrap";
+
 function PartnerList(props) {
   return (
-    <ol className={classes.list}>
-      {props.partners.map((partner) => (
-        <PartnerItem
-          key={partner.id}
-          organization={partner.organization}
-          address={partner.address}
-        />
-      ))}
-    </ol>
+    <section className={classes.list}>
+      <Table className={classes.table}>
+        <thead>
+          <tr>
+            <th>Organization</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.partners.map((partner) => (
+            <PartnerItem
+              key={partner.id}
+              organization={partner.organization}
+              address={partner.address}
+            />
+          ))}
+        </tbody>
+      </Table>
+    </section>
   );
 }
 export default PartnerList;
